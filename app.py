082aaa9487ee18683aafd6993f40cc9696a90a9b
LuisHZapata <zapataluish@gmail.com>
2026-02-02 17:35:28 -0500

    Blue print included in project

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
-import os
-
 from flask import Flask, request, jsonify, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from models import Note, db
+from notes.routes import notes_bp
 
 
 app = Flask(__name__)
-
-DB_FILE_PATH = os.path.join(os.path.dirname(__file__), "notes.sqlite")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_FILE_PATH}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f"<Note {self.id}: {self.title}>"
-
-
-@app.route("/")
-def home():
-    notes = Note.query.all()
-    return render_template("home.html", notes=notes)
+app.config.from_object(Config)
+db.init_app(app)
+app.register_blueprint(notes_bp)
 
 
 @app.route("/acerca_de")
@@ -39,58 +20,3 @@ def contact():
     if request.method == "POST":
         return "Formulario enviado correctamente", 201
     return "Pagina de contacto"
-
-
-@app.route("/api/info")
-def api_info():
-    data = {"nombre": "Notes App", "version": "1.1.1"}
-    return jsonify(data), 200
-
-
-@app.route("/confirmacion")
-def confirmation():
-    return "Su mensaje ha sido recibido."
-
-
-@app.route("/crear_nota", methods=["GET", "POST"])
-def create_note():
-    if request.method == "POST":
-        title = request.form.get("title", "")
-        content = request.form.get("content", "")
-
-        note_db = Note(title=title, content=content)
-
-        db.session.add(note_db)
-        db.session.commit()
-
-        return redirect(url_for("home"))
-
-    return render_template("note_form.html")
-
-
-@app.route("/editar-nota/<int:id>", methods=["GET", "POST"])
-def edit_note(id):
-    note = Note.query.get_or_404(id)
-
-    if request.method == "POST":
-        title = request.form.get("title", "")
-        content = request.form.get("content", "")
-
-        note.title = title
-        note.content = content
-
-        db.session.commit()
-
-        return redirect(url_for("home"))
-
-    return render_template("edit_note.html", note=note)
-
-
-@app.route("/eliminar-nota/<int:id>", methods=["POST"])
-def delete_note(id):
-    note = Note.query.get_or_404(id)
-
-    db.session.delete(note)
-    db.session.commit()
-
-    return redirect(url_for("home"))
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 DB_FILE_PATH = os.path.join(os.path.dirname(__file__), "notes.sqlite")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_FILE_PATH}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_FILE_PATH}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
